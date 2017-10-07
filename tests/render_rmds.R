01485c40683ee112c1103ca7b75ac389d49b1f9b
Naupaka Zimmerman <naupaka@gmail.com>
2017-10-07 01:47:49 -0700

    Add output for lints in addition to check

--- tests/render_rmds.R
+++ tests/render_rmds.R
@@ -3,6 +3,11 @@ f <- list.files(recursive = TRUE)
 rmds <- f[grepl(".Rmd$", f)]
 lapply(rmds, rmarkdown::render)
 
+# output lints
+for (rmdfile in rmds) {
+  lintr::lint(rmdfile)
+}
+
 # expect no lints
 for (rmdfile in rmds) {
   lintr::expect_lint(checks = NULL, file = rmdfile)
